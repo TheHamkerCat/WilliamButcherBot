2d5dae774354c1332f380c93723f7ff8a4cb8652
thehamkercat <thehamkercat@gmail.com>
2020-12-17 01:21:03 +0530

    Hard coded /repo command's reply again!

--- wbb/modules/repo.py
+++ wbb/modules/repo.py
@@ -1,4 +1,4 @@
-from wbb import app, DEVELOPER_USERNAME, REPO_URL
+from wbb import app
 from pyrogram import filters
 from wbb.utils import cust_filter
 
@@ -9,8 +9,8 @@ __HELP__ = "Do /repo To Get My Github Repository Link"
 @app.on_message(cust_filter.command(commands=(["repo"])))
 async def commit(client, message):
     await message.reply_text(f'''
-👨‍💻 Developer - {DEVELOPER_USERNAME}
+👨‍💻 Developer - @TheHamkerCat
 
-🔗 Repo - {REPO_URL}
+🔗 Repo - github.com/thehamkercat/WilliamButcherBot
 
 ❤️ PRs Are Always Welcomed''', disable_web_page_preview=True)
