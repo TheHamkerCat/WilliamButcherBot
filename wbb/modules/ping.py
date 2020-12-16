ccfc489891363fe245afb0c2daa666910a919e55
thehamkercat <thehamkercat@gmail.com>
2020-12-17 05:00:17 +0530

    Finished Ping Module

--- wbb/modules/ping.py
+++ wbb/modules/ping.py
@@ -2,23 +2,19 @@ from wbb import app
 import time
 from wbb.utils import cust_filter
 import requests
+import inspect
 __MODULE__ = "Ping"
 __HELP__ = " - /ping - to check if I am Alive or Not"
+
 
 
 @app.on_message(cust_filter.command(commands=(["ping"])))
 async def ping(client, message):
     app.set_parse_mode("markdown")
-    m = await message.reply_text("Wait, Pinging all Datacenters")
-    ping1 = round(requests.head("https://cdn1.telesco.pe/").elapsed.total_seconds() * 1000)
-    ping2 = round(requests.head("https://cdn2.telesco.pe/").elapsed.total_seconds() * 1000)
-    ping3 = round(requests.head("https://cdn3.telesco.pe/").elapsed.total_seconds() * 1000)
-    ping4 = round(requests.head("https://cdn4.telesco.pe/").elapsed.total_seconds() * 1000)
-    ping5 = round(requests.head("https://cdn5.telesco.pe/").elapsed.total_seconds() * 1000)
-    await m.edit(f'''
-```DC1 - {ping1}ms
-DC2 - {ping2}ms
-DC3 - {ping3}ms
-DC4 - {ping4}ms
-DC5 - {ping5}ms```
-''')
+    m = await message.reply_text("```Wait, Pinging all Datacenters```")
+    result = ""
+    for i in range(1, 6):
+        dc = (f"https://cdn{i}.telesco.pe")
+        ping1 = round(requests.head(dc).elapsed.total_seconds() * 1000)   
+        result += f'```DC{i} - {ping1}ms```'
+    await m.edit(result)
