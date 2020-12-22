02ea1db0350ea2f422d0302788b1a46df873751b
TheHamkerCat <thehamkercat@gmail.com>
2020-12-23 00:41:18 +0530

    Copied /promote to /demote

--- wbb/modules/admin.py
+++ wbb/modules/admin.py
@@ -10,7 +10,9 @@ __HELP__ = "/ban    - Ban A User\n" \
            "/purge  - Purge Messages\n" \
            "/del    - Delete Replied Message\n" \
            "/banme  - Bans A User Who Issued The Command\n" \
-           "/kickme - Kicks A User Who Issued The Command"
+           "/kickme - Kicks A User Who Issued The Command\n" \
+           "/demote - Demote A Member\n" \
+           "/promote - Promote A Member"
 
 SUDO = [OWNER_ID, SUDO_USER_ID]
 
@@ -286,6 +288,55 @@ async def promote(_, message: Message):
                 await message.reply_text("Yeah, I Can See You're An Admin,"
                                         +" But You Don't Have Permissions"
                                         +" To Promote Someone.")
+        else:
+            await message.reply_text("You're Not An Admin, Want A Good Ban?")
+    else:
+        await message.reply_text("Well, Your Know What?, I'M NOT AN ADMIN!"
+                                 + " MAKE ME ADMIN!")
+
+# Demote Members
+
+
+@app.on_message(cust_filter.command(commands=("demote")))
+async def demote(_, message: Message):
+    admins = await list_admins(message.chat.id)
+    chat_id = message.chat.id
+    from_user_id = message.from_user.id
+
+    if (await app.get_chat_member(chat_id,
+                                BOT_ID)).can_promote_members:
+        if message.from_user.id in admins \
+                or message.from_user.id in SUDO:
+            if (await app.get_chat_member(chat_id,
+                                        from_user_id)).can_promote_members \
+                or (await app.get_chat_member(chat_id, from_user_id)).status \
+                == 'creator' \
+                    or message.from_user.id in SUDO:
+                if message.text != '/demote':
+                    username = message.text.replace('/demote', '')
+                    user_id = (await app.get_users(username)).id
+                    await message.chat.promote_member(user_id=user_id,
+                                                    can_change_info=False,
+                                                    can_invite_users=False,
+                                                    can_restrict_members=False,
+                                                    can_delete_messages=False,
+                                                    can_pin_messages=False,
+                                                    can_promote_members=False)
+                    await message.reply_text('Promoted!')
+                else:
+                    user_id = message.reply_to_message.from_user.id
+                    await message.chat.promote_member(user_id=user_id,
+                                                    can_change_info=False,
+                                                    can_invite_users=False,
+                                                    can_restrict_members=False,
+                                                    can_delete_messages=False,
+                                                    can_pin_messages=False,
+                                                    can_promote_members=False)
+                    await message.reply_text('Demoted!')
+            else:
+                await message.reply_text("Yeah, I Can See You're An Admin,"
+                                        +" But You Don't Have Permissions"
+                                        +" To Demote Someone.")
         else:
             await message.reply_text("You're Not An Admin, Want A Good Ban?")
     else:
