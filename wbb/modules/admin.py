92f9667ea230a347bf8db2aecc357a0bb9c44d98
TheHamkerCat <thehamkercat@gmail.com>
2021-01-19 01:05:18 +0530

    various admin fixes

--- wbb/modules/admin.py
+++ wbb/modules/admin.py
@@ -94,18 +94,15 @@ async def purge(client, message: Message):
 
 @app.on_message(cust_filter.command(commands=("kick")) & ~filters.edited)
 async def kick(_, message: Message):
-    try:
-        username = (message.text.split(None, 2)[1])
-    except IndexError:
-        username = ""
     if (await app.get_chat_member(
         message.chat.id, message.from_user.id)).status == 'creator' \
         or (await app.get_chat_member(
             message.chat.id, message.from_user.id)).can_restrict_members \
             is True or message.from_user.id in SUDO:
 
-        if username != "":
-            if username in SUDO or (await app.get_users(username)).id in SUDO:
+        if len(message.command) == 2:
+            username = (message.text.split(None, 1)[1])
+            if (await app.get_users(username)).id in SUDO:
                 await message.reply_text("You Wanna Kick The Elevated One?")
             else:
                 if (await app.get_users(username)).id in \
@@ -117,9 +114,8 @@ async def kick(_, message: Message):
                     await message.reply_text("This user isn't here,"
                                              " consider kicking yourself.")
 
-        else:
-            if username in SUDO or message.reply_to_message.from_user.id \
-                    in SUDO:
+        if len(message.command) == 1 and message.reply_to_message:
+            if message.reply_to_message.from_user.id in SUDO:
                 await message.reply_text("You Wanna Kick The Elevated One?")
             else:
                 if message.reply_to_message.from_user.id in \
@@ -127,106 +123,77 @@ async def kick(_, message: Message):
                     user_id = message.reply_to_message.from_user.id
                     await message.reply_to_message.chat.kick_member(user_id)
                     await message.reply_to_message.chat.unban_member(user_id)
-                    await message.reply_text(f"Kicked {username}")
+                    await message.reply_text(f"Kicked!")
                 else:
-                    await message.reply_text("This user isn't here,"
-                                             " consider kicking yourself.")
+                    await message.reply_text("This user isn't here.")
 
 # Ban members
 
 
 @app.on_message(cust_filter.command(commands=("ban")) & ~filters.edited)
 async def ban(_, message: Message):
-    try:
-        username = (message.text.split(None, 2)[1])
-    except IndexError:
-        username = ""
     if (await app.get_chat_member(
         message.chat.id, message.from_user.id)).status == 'creator' \
         or (await app.get_chat_member(
             message.chat.id, message.from_user.id)).can_restrict_members \
             is True or message.from_user.id in SUDO:
 
-        if username != "":
-            if username in SUDO or (await app.get_users(username)).id in SUDO:
+        if len(message.command) == 2:
+            username = (message.text.split(None, 1)[1])
+            if (await app.get_users(username)).id in SUDO:
                 await message.reply_text("You Wanna Ban The Elevated One?")
             else:
                 if (await app.get_users(username)).id in \
                         await list_members(message.chat.id):
                     await message.chat.kick_member(username)
-                    await message.reply_text(f"Banned {username}")
+                    await message.reply_text(f"Banned! {username}")
                 else:
                     await message.reply_text("This user isn't here,"
                                              " consider banning yourself.")
-        else:
-            if username in SUDO or message.reply_to_message.from_user.id \
-                    in SUDO:
+
+        if len(message.command) == 1 and message.reply_to_message:
+            if message.reply_to_message.from_user.id in SUDO:
                 await message.reply_text("You Wanna Ban The Elevated One?")
             else:
-                if (await app.get_users(username)).id in \
+                if message.reply_to_message.from_user.id in \
                         await list_members(message.chat.id):
                     user_id = message.reply_to_message.from_user.id
                     await message.reply_to_message.chat.kick_member(user_id)
-                    await message.reply_text(f"Banned {username}")
+                    await message.reply_text(f"Banned!")
                 else:
-                    await message.reply_text("This user isn't here,"
-                                             " consider kicking yourself.")
+                    await message.reply_text("This user isn't here.")
+
 
 # Unban members
 
 
 @app.on_message(cust_filter.command(commands=("unban")) & ~filters.edited)
 async def unban(_, message: Message):
-    try:
-        username = (message.text.split(None, 2)[1])
-    except IndexError:
-        username = ""
     if (await app.get_chat_member(
         message.chat.id, message.from_user.id)).status == 'creator' \
         or (await app.get_chat_member(
             message.chat.id, message.from_user.id)).can_restrict_members \
             is True or message.from_user.id in SUDO:
 
-        if username != "":
+        if len(message.command) == 2:
+            username = (message.text.split(None, 1)[1])
             if (await app.get_users(username)).id not in \
                     await list_members(message.chat.id):
                 await message.chat.unban_member(username)
-                await message.reply_text(f"Unbanned {username}")
+                await message.reply_text(f"Unbanned! {username}")
             else:
                 await message.reply_text("This user is already here,"
-                                         " don't bully me!")
-        else:
-            if (await app.get_users(username)).id not in \
+                                         " consider banning yourself.")
+
+        if len(message.command) == 1 and message.reply_to_message:
+            if message.reply_to_message.from_user.id not in \
                     await list_members(message.chat.id):
                 user_id = message.reply_to_message.from_user.id
-                await message.reply_to_message.chat.unban_member(user_id)
-                await message.reply_text(f"Unbanned {username}")
+                await message.chat.unban_member(user_id)
+                await message.reply_text(f"Unbanned!")
             else:
-                await message.reply_text("This user is already here,"
-                                         " don't bully me!")
+                await message.reply_text("This user is already here.")
 
-# Kick members on their own call
-
-
-@app.on_message(cust_filter.command(commands=("kickme")) & ~filters.edited)
-async def kickme(_, message: Message):
-    if message.from_user.id not in SUDO:
-        await message.chat.kick_member(message.from_user.id)
-        await message.chat.unban_member(message.from_user.id)
-        await message.reply_text("Kicked!, Joke's on you, I'm into that shit!")
-    else:
-        await message.reply_text("It doesn't works that way mate.")
-
-# Ban members on their own call
-
-
-@app.on_message(cust_filter.command(commands=("banme")))
-async def banme(_, message: Message):
-    if message.from_user.id not in SUDO:
-        await message.chat.kick_member(message.from_user.id)
-        await message.reply_text("Banned!, Joke's on you, I'm into that shit!")
-    else:
-        await message.reply_text("It doesn't works that way mate.")
 
 # Delete messages
 
@@ -276,10 +243,7 @@ async def promote(_, message: Message):
                         user_id=user_id,
                         can_change_info=True,
                         can_invite_users=True,
-                        can_restrict_members=True,
-                        can_delete_messages=True,
-                        can_pin_messages=True,
-                        can_promote_members=True)
+                        can_delete_messages=True)
                     await message.reply_text('Promoted!')
 
                 else:
@@ -288,10 +252,7 @@ async def promote(_, message: Message):
                         user_id=user_id,
                         can_change_info=True,
                         can_invite_users=True,
-                        can_restrict_members=True,
-                        can_delete_messages=True,
-                        can_pin_messages=True,
-                        can_promote_members=True)
+                        can_delete_messages=True)
                     await message.reply_text('Promoted!')
 
             else:
@@ -321,29 +282,10 @@ async def pin(_, message: Message):
             == 'creator' \
                 or message.from_user.id in SUDO:
 
-            await message.reply_to_message.pin()
+            await message.reply_to_message.pin(disable_notification=True)
     else:
         await message.reply_text("You're Not An Admin, Stop Spamming!")
 
-# Unpin Messages
-
-
-@app.on_message(cust_filter.command(commands=("unpin")) & ~filters.edited)
-async def unpin(_, message: Message):
-    admins = await list_admins(message.chat.id)
-    chat_id = message.chat.id
-    from_user_id = message.from_user.id
-
-    if message.from_user.id in admins \
-            or message.from_user.id in SUDO:
-        if (await app.get_chat_member(chat_id,
-                                      from_user_id)).can_pin_messages \
-            or (await app.get_chat_member(chat_id, from_user_id)).status \
-            == 'creator' \
-                or message.from_user.id in SUDO:
-            await app.unpin_all_chat_messages(chat_id)
-    else:
-        await message.reply_text("You're Not An Admin, Stop Spamming!")
 
 # Mute members
 
@@ -351,15 +293,17 @@ async def unpin(_, message: Message):
 @app.on_message(cust_filter.command(commands=("mute")) & ~filters.edited)
 async def mute(_, message: Message):
     chat_id = message.chat.id
-    admins = await list_admins(chat_id)
     from_user_id = message.from_user.id
-    username = message.text.replace("/mute", "")
+    if not message.reply_to_message:
+        await message.reply_text("Reply To A User's Message!")
+        return
 
-    if from_user_id in admins or from_user_id in SUDO:
-        if username != "":
-            victim = username
-        else:
-            victim = message.reply_to_message.from_user.id
+    if (await app.get_chat_member(chat_id,
+                                  from_user_id)).can_restrict_members \
+        or (await app.get_chat_member(chat_id, from_user_id)).status \
+        == 'creator' \
+            or message.from_user.id in SUDO:
+        victim = message.reply_to_message.from_user.id
         await message.chat.restrict_member(victim,
                                            permissions=ChatPermissions())
         await message.reply_text("Muted!")
@@ -372,15 +316,16 @@ async def mute(_, message: Message):
 @app.on_message(cust_filter.command(commands=("unmute")) & ~filters.edited)
 async def unmute(_, message: Message):
     chat_id = message.chat.id
-    admins = await list_admins(chat_id)
     from_user_id = message.from_user.id
-    username = message.text.replace("/unmute", "")
-
-    if from_user_id in admins or from_user_id in SUDO:
-        if username != "":
-            victim = username
-        else:
-            victim = message.reply_to_message.from_user.id
+    if not message.reply_to_message:
+        await message.reply_text("Reply To A User's Message!")
+        return
+    if (await app.get_chat_member(chat_id,
+                                  from_user_id)).can_restrict_members \
+        or (await app.get_chat_member(chat_id, from_user_id)).status \
+        == 'creator' \
+            or message.from_user.id in SUDO:
+        victim = message.reply_to_message.from_user.id
         await message.chat.unban_member(victim)
         await message.reply_text("Unmuted!")
     else:
