2e98b7325059c6472a3ace090f2b00c968a50d12
thehamkercat <thehamkercat@gmail.com>
2021-03-07 12:45:21 +0530

    startup message

--- wbb/utils/botinfo.py
+++ wbb/utils/botinfo.py
@@ -1,3 +1,4 @@
+from wbb import LOG_GROUP_ID
 BOT_ID = 0
 BOT_NAME = ""
 BOT_USERNAME = ""
@@ -8,9 +9,12 @@ async def get_info(app):
     global BOT_ID, BOT_NAME, BOT_USERNAME, BOT_DC_ID
     getme = await app.get_me()
     BOT_ID = getme.id
+    
     if getme.last_name:
         BOT_NAME = getme.first_name + " " + getme.last_name
     else:
         BOT_NAME = getme.first_name
     BOT_USERNAME = getme.username
     BOT_DC_ID = getme.dc_id
+    try:
+        await app.send_message(LOG_GROUP_ID, text=f"{BOT_NAME} Started!")
