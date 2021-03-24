05886c9e20f498053397b4990b9108a6367cca0c
TheHamkerCat <thehamkercat@gmail.com>
2021-03-24 11:05:29 +0530

    a

--- wbb/utils/dbfunctions.py
+++ wbb/utils/dbfunctions.py
@@ -144,14 +144,6 @@ async def int_to_alpha(user_id: int) -> str:
         text += alphabet[int(i)]
     return text
 
-async def int_to_alpha_chat(chat_id: int) -> str:
-    alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
-    text = ""
-    chat_id = str(chat_id)
-    chat_id = chat_id.replace("-", "")
-    for i in chat_id:
-        text += alphabet[int(i)]
-    return text
 
 async def alpha_to_int(user_id_alphabet: str) -> int:
     alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
@@ -161,16 +153,6 @@ async def alpha_to_int(user_id_alphabet: str) -> int:
         user_id += str(index)
     user_id = int(user_id)
     return user_id
-
-
-async def alpha_to_int_chat(chat_id_alphabet: str) -> int:
-    alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
-    chat_id = "-"
-    for i in chat_id_alphabet:
-        index = alphabet.index(i)
-        chat_id += str(index)
-    chat_id = int(chat_id)
-    return chat_id
 
 
 async def get_warns(chat_id: int) -> Dict[str, int]:
@@ -260,46 +242,26 @@ async def update_karma(chat_id: int, name: str, karma: dict):
 """ Chats log functions """
 
 async def is_served_chat(chat_id: int) -> bool:
-    chats = await chatsdb.find_one({"chats": "chats"})
-    if not chats: return False
-    chats = chats['chats']
-    for chat in chats:
-        if chat == await int_to_alpha_chat(chat_id): return True
+    chat = await chatsdb.find_one({"chat_id": chat_id})
+    if not chat: return False
+    return True
 
 
-async def get_served_chats():
-    chats = await chatsdb.find_one({"chats": "chats"})
-    if not chats: return {}
-    chats = chats['chats']
-    return chats
+async def get_served_chats() -> list:
+    chats = await chatsdb.find({"chat_id": {'$gt': 0}})
+    if not chats: return []
+    chats_list = []
+    for chat in await chats.to_list(length=1000000000):
+        chats_list.append(chat)
+    return chats_list
 
 
 async def add_served_chat(chat_id: int):
     is_served = await is_served_chat(chat_id)
     if is_served: return
-    chats = await get_served_chats()
-    chats[await int_to_alpha_chat(chat_id)] = {"chats": "chats"}
-    return await chatsdb.update_one(
-            {"chats": "chats"},
-            {
-                "$set": {
-                    "chats": chats
-                }
-            },
-            upsert=True
-            )
+    return await chatsdb.insert_one({"chat_id": chat_id})
 
 async def remove_served_chat(chat_id: int):
     is_served = await is_served_chat(chat_id)
     if not is_served: return
-    chats = await get_served_chats()
-    del chats[await int_to_alpha_chat(chat_id)]
-    return await chatsdb.update_one(
-            {"chats": "chats"},
-            {
-                "$set": {
-                    "chats": chats
-                }
-            },
-            upsert=True
-            )
+    await chatsdb.delete_one({"chat_id": chat_id})
