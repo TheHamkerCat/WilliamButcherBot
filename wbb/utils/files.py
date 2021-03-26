c09e33246fbdf87b6d72fa488023f097b1093ebf
Sylphy <79474084+sylphiette@users.noreply.github.com>
2021-03-26 17:11:55 +0300

    Rewrite of the kang module (v2) (#55)
    * Rewrite of the kang (stickers) module.
    I changed the module to use the pyrogram's built-in raw functions
    
    > I fixed a lot of problems and ended up rewriting it
    
    * Small fixes to the kang module
    
    * More fixes in the kang module
    
    * Small fix regarding file formats in the kang module

---
+++ wbb/utils/files.py
@@ -0,0 +1,56 @@
+import os
+from PIL import Image
+import math
+
+from pyrogram import Client, raw
+from pyrogram.file_id import FileId
+
+STICKER_DIMENSIONS = (512, 512)
+
+
+async def resize_file_to_sticker_size(file_path: str) -> str:
+    im = Image.open(file_path)
+    if (im.width, im.height) < STICKER_DIMENSIONS:
+        size1 = im.width
+        size2 = im.height
+        if im.width > im.height:
+            scale = STICKER_DIMENSIONS[0]/size1
+            size1new = STICKER_DIMENSIONS[0]
+            size2new = size2 * scale
+        else:
+            scale = STICKER_DIMENSIONS[1]/size2
+            size1new = size1 * scale
+            size2new = STICKER_DIMENSIONS[1]
+        size1new = math.floor(size1new)
+        size2new = math.floor(size2new)
+        sizenew = (size1new, size2new)
+        im = im.resize(sizenew)
+    else:
+        im.thumbnail(STICKER_DIMENSIONS)
+    try:
+        os.remove(file_path)
+        file_path = f"{file_path}.png"
+        return file_path
+    finally:
+        im.save(file_path)
+
+async def upload_document(client: Client, file_path: str, chat_id: int) -> raw.base.InputDocument:
+    media = await client.send(
+        raw.functions.messages.UploadMedia(
+            peer=await client.resolve_peer(chat_id),
+            media=raw.types.InputMediaUploadedDocument(
+                mime_type=client.guess_mime_type(file_path) or "application/zip",
+                file=await client.save_file(file_path),
+                attributes=[
+                    raw.types.DocumentAttributeFilename(
+                        file_name=os.path.basename(file_path)
+                    )
+                ]
+            )
+        )
+    )
+    return raw.types.InputDocument(id=media.document.id, access_hash=media.document.access_hash, file_reference=media.document.file_reference)
+
+async def get_document_from_file_id(file_id: str) -> raw.base.InputDocument:
+    decoded = FileId.decode(file_id)
+    return raw.types.InputDocument(id=decoded.media_id, access_hash=decoded.access_hash, file_reference=decoded.file_reference)
---
+++ wbb/utils/stickerset.py
@@ -0,0 +1,38 @@
+from typing import List
+
+from pyrogram import Client, raw
+from pyrogram import errors
+
+async def get_sticker_set_by_name(client: Client, name: str) -> raw.base.messages.StickerSet:
+    try:
+        return await client.send(
+            raw.functions.messages.GetStickerSet(
+                stickerset = raw.types.InputStickerSetShortName(short_name=name)
+            )
+        )
+    except errors.exceptions.not_acceptable_406.StickersetInvalid:
+        return None
+
+# Known errors: (I don't see a reason to catch them as we, for sure, won't face them right now):
+# errors.exceptions.bad_request_400.PackShortNameInvalid -> pack name needs to end with _by_botname
+# errors.exceptions.bad_request_400.ShortnameOccupyFailed -> pack's name is already in use
+async def create_sticker_set(client: Client, owner: int, title: str, short_name: str, stickers: List[raw.base.InputStickerSetItem]) -> raw.base.messages.StickerSet:
+    return await client.send(
+        raw.functions.stickers.CreateStickerSet(
+            user_id=await client.resolve_peer(owner), 
+            title=title,
+            short_name=short_name,
+            stickers=stickers
+        )
+    )
+
+async def add_sticker_to_set(client: Client, stickerset: raw.base.messages.StickerSet, sticker: raw.base.InputStickerSetItem) -> raw.base.messages.StickerSet:
+    return await client.send(
+        raw.functions.stickers.AddStickerToSet(
+            stickerset=raw.types.InputStickerSetShortName(short_name=stickerset.set.short_name),
+            sticker=sticker
+        )
+    )
+
+async def create_sticker(sticker: raw.base.InputDocument, emoji: str) -> raw.base.InputStickerSetItem:
+    return raw.types.InputStickerSetItem(document=sticker, emoji=emoji)
