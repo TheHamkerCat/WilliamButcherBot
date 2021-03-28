7b9a38b7f78c633970f4b0bf089a055aedc5cc3f
TheHamkerCat <thehamkercat@gmail.com>
2021-03-29 02:46:45 +0530

    a

--- wbb/modules/sudoers.py
+++ wbb/modules/sudoers.py
@@ -273,5 +273,5 @@ async def broadcast_message(_, message):
 
 @app.on_message(filters.command("update") & filters.user(SUDOERS))
 async def update_restart(_, message):
-    await message.reply_text(subprocess.check_output(["git", "pull"]).decode("UTF-8"))
+    await message.reply_text(f'```{subprocess.check_output(["git", "pull"]).decode("UTF-8")}```')
     os.execvp("python3", ["python3", "-m", "wbb"])
