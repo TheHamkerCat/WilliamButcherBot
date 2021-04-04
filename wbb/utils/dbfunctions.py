c2f1bfe9b7dd89bc2ef597e17fd85ae761cd7373
TheHamkerCat <thehamkercat@gmail.com>
2021-04-04 10:04:26 +0530

    Captcha a

--- wbb/utils/dbfunctions.py
+++ wbb/utils/dbfunctions.py
@@ -405,20 +405,20 @@ async def save_couple(chat_id: int, date: str, couple: dict):
 async def is_captcha_on(chat_id: int) -> bool:
     chat = await captchadb.find_one({"chat_id": chat_id})
     if not chat:
-        return False
-    return True
+        return True
+    return False
 
 
 async def captcha_on(chat_id: int):
     is_captcha = await is_captcha_on(chat_id)
     if is_captcha:
         return
-    return await captchadb.insert_one({"chat_id": chat_id})
+    return await captchadb.delete_one({"chat_id": chat_id})
 
 
 async def captcha_off(chat_id: int):
     is_captcha = await is_captcha_on(chat_id)
     if not is_captcha:
         return
-    return await captchadb.delete_one({"chat_id": chat_id})
+    return await captchadb.insert_one({"chat_id": chat_id})
 
