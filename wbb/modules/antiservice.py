65d356e2e7fbe62f5847e51d610bbf6191ff8bb4
sᴡᴏɴɪᴛ <swatv3nub@gmail.com>
2021-04-13 14:20:31 +0530

    Updated AntiServiceSystem
    Added Dedicated Database functions

--- wbb/modules/antiservice.py
+++ wbb/modules/antiservice.py
@@ -3,7 +3,8 @@
 
 from wbb import app
 from pyrogram import filters
-from wbb.modules.admin import list_admins as admemez
+from wbb.modules.admin import member_permissions
+from wbb.utils.dbfunctions import is_antiservice_on, antiservice_on, antiservice_off
 
 __MODULE__ = "AntiService"
 __HELP__ = f"""
@@ -12,33 +13,25 @@ Plugin to delete antiservice messages in a chat!
 /antiservice [enable|disable]
 """
 
-active_chats = []
-
-@app.on_message(filters.command("antiservice") & ~filters.edited)
+@app.on_message(filters.command("antiservice") & ~filters.private)
 async def anti_service(_, message):
-    global active_chats
-    if message.from_user.id not in admemez:
-        await message.reply_text("You are Not an Admin!")
-        return
     if len(message.command) != 2:
-        await message.reply_text("/antiservice [enable | disable]")
+        await message.reply_text("Usage: /antiservice [enable | disable]")
         return
-    status = message.text.split(None, 1)[1]
+    status = message.text.split(None, 1)[1].strip()
+    status = status.lower()
     chat_id = message.chat.id
-    
-    if status == "ENABLE" or status == "enable" or status == "Enable":
-        if chat_id not in active_chats:
-            active_chats.append(chat_id)
-            await message.reply_text("Enabled AntiService System. I will Be Deleting Every Service Message from Now on.")
-            return
-        await message.reply_text("AntiService System is Already Enabled")
-    
-    elif status == "DISABLE" or status == "disable" or status == "Disable":
-        if chat_id in active_chats:
-            active_chats.remove(chat_id)
-            await message.reply_text("Disabled AntiService System. I won't Be Deleting Service Message from Now on.")
-            return
-            await message.reply_text("AntiService System was Never Enabled! How can I Disable it?")
+    user_id = message.from_user.id
+    permissions = await member_permissions(chat_id, user_id)
+    if "can_change_info" not in permissions:
+        await message.reply_text("You don't have enough permissions.")
+        return
+    if status == "enable":
+        await antiservice_on(chat_id)
+        await message.reply_text("Enabled AntiService System. I will Delete Service Messages from Now on.")
+    elif status == "disable":
+        await antiservice_off(chat_id)
+        await message.reply_text("Disabled AntiService System. I won't Be Deleting Service Message from Now on.")
     else:
         await message.reply_text("Unknown Suffix, Use /antiservice [enable|disable]")
 
@@ -48,7 +41,7 @@ async def anti_service(_, message):
 async def delete_service(_, message):
     chat_id = message.chat.id
     try:
-        if chat_id in active_chats:
+        if await is_antiservice_on(chat_id):
             await message.delete()
             return
     except:
