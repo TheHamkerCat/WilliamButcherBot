496690face4fb52f6d74e2a7f89dc37f339cf380
TheHamkerCat <thehamkercat@gmail.com>
2021-04-17 18:22:32 +0530

    Removed some shit

--- wbb/modules/images.py
+++ wbb/modules/images.py
@@ -8,7 +8,8 @@ from wbb.utils.errors import capture_err
 
 __MODULE__ = "Images"
 __HELP__ = '''/cat  - Get Cute Cats Images
-/wall - Get Wallpapers'''
+For more images like wallpapers, use inline mode.
+send /inline for inline help.'''
 
 
 async def delete_message_with_delay(delay, message):
@@ -25,28 +26,3 @@ async def cat(_, message):
         data = json.loads(url.read().decode())
     cat_url = (data[0]['url'])
     await message.reply_photo(cat_url)
-
-
-@app.on_message(filters.command("wall") & ~filters.edited)
-@capture_err
-async def wall(_, message):
-    if len(message.command) < 2:
-        await message.reply_text("/wall needs an argument")
-        return
-    initial_term = message.text.split(None, 1)[1]
-    m = await message.reply_text("Searching!")
-    term = initial_term.replace(' ', '%20')
-    try:
-        wallpapers = await arq.wall(term)
-    except KeyError:
-        await m.edit("Found literally nothing!,"
-                     + "You should work on your English.")
-        return
-    if len(wallpapers) > 10:
-        selection = 10
-    else:
-        selection = len(wallpapers)
-    index = randint(0, selection - 1)
-    wallpaper = wallpapers[index].url_image
-    await message.reply_document(wallpaper)
-    await m.delete()
