90363f6eaf7693372afbb5ce91e7f69593e80906
thehamkercat <thehamkercat@gmail.com>
2021-05-28 21:57:02 +0530

    a

--- wbb/modules/filters.py
+++ wbb/modules/filters.py
@@ -130,6 +130,7 @@ async def filters_re(_, message):
                     )
                     if text[0] == "~":
                         return await message.delete()
+                return
                 await message.reply_text(data, disable_web_page_preview=True)
             else:
                 if message.reply_to_message:
