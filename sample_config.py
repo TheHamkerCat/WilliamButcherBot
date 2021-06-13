22dacb9dc0c77583e740154039452a7979d9e927
Tejas61900 <73832085+Tejas61900@users.noreply.github.com>
2021-06-13 21:00:27 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,7 +1,7 @@
 from dotenv import load_dotenv
 load_dotenv('config.env')
 
-HEROKU = True  # NOTE Make it false if you're not deploying on heroku.
+HEROKU = True  # NOTE Make it false if you're not deploying on heroku or docker.
 
 if HEROKU:
     from os import environ
