482002f96bac3717465643e63efebc5006777a57
Ihsan <78595134+UserLazy@users.noreply.github.com>
2021-06-29 14:32:28 +0700

    Create musicplayer.py

---
+++ wbb/modules/musicplayer.py
@@ -0,0 +1,60 @@
+# Copyright (C) 2021 TeamDaisyX
+
+
+# This file is part of Daisy (Telegram Bot)
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+__mod_name__ = "Music Player"
+__help__ = """
+          <b>》** OdaXMusic ** 《</b>
+            
+ OdaXMusic plays plays music in your group's voice chat
+ 
+ Assistant name >> @OdaHelper
+ 
+<b> Setting up </b>
+
+1) Make bot admin
+2) Start a voice chat
+3) Try `/play [song name]` for the first time by an admin
+*) If userbot joined enjoy music, If not add @OdaHelper to your group and retry
+
+<b> Commands </b>
+
+=>> Song Playing 🎧 
+
+  /play: Play song using youtube music
+  /play [yt url] : Play the given yt url
+  
+  
+=>> Playback ⏯
+
+  /player: Open Settings menu of player
+  /skip: Skips the current track
+  /pause: Pause track
+  /resume: Resumes the paused track
+  /end: Stops media playback
+  /current: Shows the current Playing track
+  /playlist: Shows playlist
+
+=>> More tools 🧑‍🔧
+
+  /admincache: Updates admin info of your group. Try if bot isn't recognize admin
+  /userbotjoin: Invite @OdaHelper Userbot to your chat
+
+*Player cmd and all other cmds except /play, /current  and /playlist  are only for admins with manage group
+
+<b> PLEASE NOTE THIS SERVICE IS UNSTABLE AND CAN BE STOPPED ANYTIME </b>
+"""
