3b095ef42adf5d858d0746ad20e9d6954360202d
TheHamkerCat <thehamkercat@gmail.com>
2021-07-06 02:01:25 +0530

    Module def

--- wbb/modules/proxy.py
+++ wbb/modules/proxy.py
@@ -6,6 +6,10 @@ from pyrogram.types import CallbackQuery, Message
 from wbb import app, arq
 from wbb.core.keyboard import ikb
 
+__MODULE__ = "Proxy"
+__HELP__ = "/proxy - Get socks5 proxy which you can" \
+           + " use with telegram or other things"
+
 proxies = []
 
 
