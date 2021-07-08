00f687568ffde8923c05bd58632902c7869bdb40
iamajneya <87112487+iamajneya@users.noreply.github.com>
2021-07-08 12:09:48 +0530

    create chats(#82)
    * just create
    
    [basic|super]group & channel
    
    * create help

---
+++ wbb/modules/create.py
@@ -0,0 +1,44 @@
+from pyrogram import filters
+from wbb import BOT_USERNAME, SUDOERS, USERBOT_PREFIX, app2
+from wbb.modules.userbot import edit_or_reply
+
+
+@app2.on_message(
+    filters.user(SUDOERS)
+    & ~filters.forwarded
+    & ~filters.via_bot
+    & ~filters.edited
+    & filters.command("create", prefixes=USERBOT_PREFIX)
+)
+async def create(_, message):
+    if len(message.command) < 3:
+        return await edit_or_reply(
+            message, text="__**.create (b|s|c) Name**__"
+        )
+    group_type = message.command[1]
+    split = message.command[2:]
+    group_name = " ".join(split)
+    desc = "Welcome To My " + (
+        "Supergroup" if group_type == "s" else "Channel"
+    )
+    if group_type == "b":  # for basicgroup
+        _id = await app2.create_group(group_name, BOT_USERNAME)
+        link = await app2.get_chat(_id["id"])
+        await edit_or_reply(
+            message,
+            text=f"**Basicgroup Created: [{group_name}]({link['invite_link']})**",
+        )
+    elif group_type == "s":  # for supergroup
+        _id = await app2.create_supergroup(group_name, desc)
+        link = await app2.get_chat(_id["id"])
+        await edit_or_reply(
+            message,
+            text=f"**Supergroup Created: [{group_name}]({link['invite_link']})**",
+        )
+    elif group_type == "c":  # for channel
+        _id = await app2.create_channel(group_name, desc)
+        link = await app2.get_chat(_id["id"])
+        await edit_or_reply(
+            message,
+            text=f"**Channel Created: [{group_name}]({link['invite_link']})**",
+        )
