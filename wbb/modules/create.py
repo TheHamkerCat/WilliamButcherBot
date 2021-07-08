4dfdc31a0b7f85bf68eb030f565e9e372a3f06f5
Akshay Rajput <thehamkercat@gmail.com>
2021-07-08 12:08:42 +0530

    Format

--- wbb/modules/create.py
+++ wbb/modules/create.py
@@ -1,7 +1,6 @@
-from time import sleep
 from pyrogram import filters
+from wbb import BOT_USERNAME, SUDOERS, USERBOT_PREFIX, app2
 from wbb.modules.userbot import edit_or_reply
-from wbb import SUDOERS, USERBOT_PREFIX, BOT_USERNAME, app2
 
 
 @app2.on_message(
@@ -12,25 +11,34 @@ from wbb import SUDOERS, USERBOT_PREFIX, BOT_USERNAME, app2
     & filters.command("create", prefixes=USERBOT_PREFIX)
 )
 async def create(_, message):
-    if len(message.command) >= 3:
-        group_type = message.command[1]
-        split = message.command[2:]
-        group_name = ' '.join(split)
-        desc = "Welcome To My " + \
-            ("Supergroup" if group_type == "s" else "Channel")
-        if group_type == "b":  # for basicgroup
-            _id = await app2.create_group(group_name, BOT_USERNAME)
-            link = await app2.get_chat(_id['id'])
-            await edit_or_reply(message, text=f"**Basicgroup Created: [{group_name}]({link['invite_link']})**")
-        elif group_type == "s":  # for supergroup
-            _id = await app2.create_supergroup(group_name, desc)
-            link = await app2.get_chat(_id['id'])
-            await edit_or_reply(message, text=f"**Supergroup Created: [{group_name}]({link['invite_link']})**")
-        elif group_type == "c":  # for channel
-            _id = await app2.create_channel(group_name, desc)
-            link = await app2.get_chat(_id['id'])
-            await edit_or_reply(message, text=f"**Channel Created: [{group_name}]({link['invite_link']})**")
-    else:
-        await edit_or_reply(message, text="__**.create (b|s|c) Name**__")
-    sleep(10)
-    await message.delete()
+    if len(message.command) < 3:
+        return await edit_or_reply(
+            message, text="__**.create (b|s|c) Name**__"
+        )
+    group_type = message.command[1]
+    split = message.command[2:]
+    group_name = " ".join(split)
+    desc = "Welcome To My " + (
+        "Supergroup" if group_type == "s" else "Channel"
+    )
+    if group_type == "b":  # for basicgroup
+        _id = await app2.create_group(group_name, BOT_USERNAME)
+        link = await app2.get_chat(_id["id"])
+        await edit_or_reply(
+            message,
+            text=f"**Basicgroup Created: [{group_name}]({link['invite_link']})**",
+        )
+    elif group_type == "s":  # for supergroup
+        _id = await app2.create_supergroup(group_name, desc)
+        link = await app2.get_chat(_id["id"])
+        await edit_or_reply(
+            message,
+            text=f"**Supergroup Created: [{group_name}]({link['invite_link']})**",
+        )
+    elif group_type == "c":  # for channel
+        _id = await app2.create_channel(group_name, desc)
+        link = await app2.get_chat(_id["id"])
+        await edit_or_reply(
+            message,
+            text=f"**Channel Created: [{group_name}]({link['invite_link']})**",
+        )
