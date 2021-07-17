e1bebd96dadaaaa89dda8954d6f3631462bbb26c
iamajneya <87112487+iamajneya@users.noreply.github.com>
2021-07-17 17:15:40 +0530

    k

--- wbb/modules/notes.py
+++ wbb/modules/notes.py
@@ -23,7 +23,6 @@ from wbb import app
 from wbb.core.decorators.errors import capture_err
 from wbb.core.decorators.permissions import adminsOnly
 from wbb.core.keyboard import revert_button, build_button
-from wbb.utils.functions import button_parser
 from wbb.utils.msg_types import get_msg_file
 from wbb.utils.dbfunctions import (delete_note, get_note,
                                    get_note_names, save_note)
