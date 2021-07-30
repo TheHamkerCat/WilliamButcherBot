f760999e754ccb2548ee87c5acb411d85657cd61
Cypher Izumi <85168398+IzumiCypherX@users.noreply.github.com>
2021-07-30 22:38:51 +0530

    Update couple.py

--- wbb/modules/couple.py
+++ wbb/modules/couple.py
@@ -31,7 +31,7 @@ from wbb.core.decorators.errors import capture_err
 from wbb.utils.dbfunctions import get_couple, save_couple
 
 __MODULE__ = "Shippering"
-__HELP__ = "/detect_gay - To Choose Couple Of The Day"
+__HELP__ = "/shippering - To Choose Couple Of The Day"
 
 
 # Date and time
@@ -57,7 +57,7 @@ today = str(dt()[0])
 tomorrow = str(dt_tom())
 
 
-@app.on_message(filters.command("detect_gay") & ~filters.edited)
+@app.on_message(filters.command("shippering") & ~filters.edited)
 @capture_err
 async def couple(_, message):
     if message.chat.type == "private":
