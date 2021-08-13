8564636e9f801c1a115a6db742297f5f423167d8
TheHamkerCat <thehamkercat@gmail.com>
2021-08-13 20:29:05 +0530

    Row_width

--- wbb/core/keyboard.py
+++ wbb/core/keyboard.py
@@ -52,4 +52,4 @@ def ikb(data: dict, row_width: int = 2):
     Converts a dict to pyrogram buttons
     Ex: dict_to_keyboard({"click here": "this is callback data"})
     """
-    return keyboard(data.items(), row_width=2)
+    return keyboard(data.items(), row_width=row_width)
