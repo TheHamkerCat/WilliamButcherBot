d072f285894cffd1b0fccd4105171981e239032d
Akshay Rajput <thehamkercat@gmail.com>
2021-09-10 09:03:23 +0530

    `return x` instead or `x \nreturn`, looks clean

--- wbb/modules/admin.py
+++ wbb/modules/admin.py
@@ -347,8 +347,8 @@ async def promoteFunc(_, message: Message):
         can_manage_chat=bot.can_manage_chat,
         can_manage_voice_chats=bot.can_manage_voice_chats,
     )
-        await message.reply_text("Promoted! With All Rights")
-        return
+        return await message.reply_text("Promoted! With All Rights")
+
     await message.chat.promote_member(
         user_id=user_id,
         can_change_info=bot.can_change_info,
