8f6e7ff6c7301553a603d768188a17255db67fa4
Cypher Izumi <85168398+IzumiCypherX@users.noreply.github.com>
2021-10-14 20:20:06 +0530

    Fix Typo
    Assuming it a typo, Fixed it.
    Dunno if its Intended

--- wbb/modules/karma.py
+++ wbb/modules/karma.py
@@ -40,7 +40,7 @@ __HELP__ = """[UPVOTE] - Use upvote keywords like "+", "+1", "thanks" etc to upv
 [DOWNVOTE] - Use downvote keywords like "-", "-1", etc to downvote a message.
 /karma_toggle [ENABLE|DISABLE] - Enable or Disable Karma System In Your Chat.
 Reply to a message with /karma to check a user's karma
-Send /karma without replying to any message to chek karma list of top 10 users"""
+Send /karma without replying to any message to check karma list of top 10 users"""
 
 
 regex_upvote = r"^(\+|\+\+|\+1|thx|tnx|ty|thank you|thanx|thanks|pro|cool|good|👍|\+\+ .+)$"
