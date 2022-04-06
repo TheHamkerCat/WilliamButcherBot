ff77bc9934131ba1593b65c4b99944c52765e15a
TheHamkerCat <TheHamkerCat@gmail.com>
2022-04-06 15:17:33 +0530

    Add .reserve command to reserve usernames

--- wbb/modules/userbot.py
+++ wbb/modules/userbot.py
@@ -233,3 +233,27 @@ async def shellrunner(_, message: Message):
             message,
             text=f"**INPUT:**\n```{escape(text)}```\n\n**OUTPUT: **\n`No output`",
         )
+
+
+@app2.on_message(
+    SUDOERS
+    & filters.command("reserve", prefixes=USERBOT_PREFIX)
+    & ~filters.edited
+)
+async def reserve_channel_handler(_, message: Message):
+    if len(message.text.split()) != 2:
+        return await eor(message, text="Pass a username as argument!!")
+
+    username = message.text.split(None, 1)[1].strip().replace("@", "")
+
+    m = await eor(message, text="Reserving...")
+
+    chat = await app2.create_channel(
+        username, "Created by .reserve command"
+    )
+    try:
+        await app2.update_chat_username(chat.id, username)
+    except Exception as e:
+        await m.edit(f"Couldn't Reserve, Error: `{str(e)}`")
+        return await app2.delete_channel(chat.id)
+    await m.edit(f"Reserved @{username} Successfully")
