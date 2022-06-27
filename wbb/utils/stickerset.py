7d99850171cc791dd4b6d7204c653a54699eed3c
Hunter-XD <technicalhunter0000@gmail.com>
2022-06-27 13:21:57 +0530

    Fix sticker

--- wbb/utils/stickerset.py
+++ wbb/utils/stickerset.py
@@ -30,7 +30,7 @@ async def get_sticker_set_by_name(
         client: Client, name: str
 ) -> raw.base.messages.StickerSet:
     try:
-        return await client.send(
+        return await client.invoke(
             raw.functions.messages.GetStickerSet(
                 stickerset=raw.types.InputStickerSetShortName(short_name=name),
                 hash=0,
@@ -52,7 +52,7 @@ async def create_sticker_set(
         short_name: str,
         stickers: List[raw.base.InputStickerSetItem],
 ) -> raw.base.messages.StickerSet:
-    return await client.send(
+    return await client.invoke(
         raw.functions.stickers.CreateStickerSet(
             user_id=await client.resolve_peer(owner),
             title=title,
@@ -67,7 +67,7 @@ async def add_sticker_to_set(
         stickerset: raw.base.messages.StickerSet,
         sticker: raw.base.InputStickerSetItem,
 ) -> raw.base.messages.StickerSet:
-    return await client.send(
+    return await client.invoke(
         raw.functions.stickers.AddStickerToSet(
             stickerset=raw.types.InputStickerSetShortName(
                 short_name=stickerset.set.short_name
