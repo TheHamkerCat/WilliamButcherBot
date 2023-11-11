5b72f233eb37cfd5a566c865f2f529d2c7d96d00
SI_NN_ER_LS <72606082+si-nn-er-ls@users.noreply.github.com>
2023-11-11 13:37:38 +0530

    added: support to all kinda notes (#225)

--- wbb/modules/notes.py
+++ wbb/modules/notes.py
@@ -35,7 +35,11 @@ from wbb.utils.functions import extract_text_and_keyb
 __MODULE__ = "Notes"
 __HELP__ = """/notes To Get All The Notes In The Chat.
 
-/save [NOTE_NAME] To Save A Note (Can be a sticker or text).
+/save [NOTE_NAME] To Save A Note.
+
+Supported note types are Text, Animation, Photo, Document, Video, video notes, Audio, Voice
+
+To change caption of any files use.\n/save [NOTE_NAME] [NEW_CAPTION]
 
 #NOTE_NAME To Get A Note.
 
@@ -44,6 +48,17 @@ __HELP__ = """/notes To Get All The Notes In The Chat.
 Checkout /markdownhelp to know more about formattings and other syntax.
 """
 
+
+def extract_urls(reply_markup):
+    urls = []
+    if reply_markup.inline_keyboard:
+        buttons = reply_markup.inline_keyboard
+        for i, row in enumerate(buttons):
+            for j, button in enumerate(row):
+                 if button.url:
+                    name = "\n~\nbutton" if i * len(row) + j + 1 == 1 else f"button{i * len(row) + j + 1}"
+                    urls.append((f"{name}", button.text, button.url))
+    return urls
 
 @app2.on_message(
     filters.command("save", prefixes=USERBOT_PREFIX)
@@ -57,24 +72,63 @@ async def save_notee(_, message):
     if len(message.command) < 2 or not message.reply_to_message:
         await eor(
             message,
-            text="**Usage:**\nReply to a text or sticker with /save [NOTE_NAME] to save it.",
-        )
-
-    elif not message.reply_to_message.text and not message.reply_to_message.sticker:
-        await eor(
-            message,
-            text="__**You can only save text or stickers in notes.**__",
+            text="**Usage:**\nReply to a message with /save [NOTE_NAME] to save a new note.",
         )
     else:
         name = message.text.split(None, 1)[1].strip()
         if not name:
             return await eor(message, text="**Usage**\n__/save [NOTE_NAME]__")
-        _type = "text" if message.reply_to_message.text else "sticker"
+        replied_message = message.reply_to_message
+        text = name.split(" ", 1)
+        if len(text) > 1:
+            name = text[0]
+            data = text[1].strip()
+            if replied_message.sticker or replied_message.video_note:
+                data = None
+        else:
+            if replied_message.sticker or replied_message.video_note:
+                data = None
+            elif not replied_message.text and not replied_message.caption:
+                data = None
+            else:
+                data = replied_message.text.markdown if replied_message.text else replied_message.caption.markdown
+        if replied_message.text:
+            _type = "text"
+            file_id = None
+        if replied_message.sticker:
+            _type = "sticker"
+            file_id = replied_message.sticker.file_id
+        if replied_message.animation:
+            _type = "animation"
+            file_id = replied_message.animation.file_id
+        if replied_message.photo:
+            _type = "photo"
+            file_id = replied_message.photo.file_id
+        if replied_message.document:
+            _type = "document"
+            file_id = replied_message.document.file_id
+        if replied_message.video:
+            _type = "video"
+            file_id = replied_message.video.file_id
+        if replied_message.video_note:
+            _type = "video_note"
+            file_id = replied_message.video_note.file_id
+        if replied_message.audio:
+            _type = "audio"
+            file_id = replied_message.audio.file_id
+        if replied_message.voice:
+            _type = "voice"
+            file_id = replied_message.voice.file_id
+        if replied_message.reply_markup:
+            if not "~" in data:
+                urls = extract_urls(replied_message.reply_markup)
+                if urls:
+                    response = "\n".join([f"{name}=[{text}, {url}]" for name, text, url in urls])
+                    data = data + response
         note = {
             "type": _type,
-            "data": message.reply_to_message.text.markdown
-            if _type == "text"
-            else message.reply_to_message.sticker.file_id,
+            "data": data,
+            "file_id": file_id,
         }
         prefix = message.text.split()[0][0]
         chat_id = message.chat.id if prefix != USERBOT_PREFIX else USERBOT_ID
@@ -142,21 +196,69 @@ async def get_one_note(_, message):
     _note = await get_note(message.chat.id, name)
     if not _note:
         return
-    if _note["type"] == "text":
-        data = _note["data"]
-        keyb = None
+    type = _note["type"]
+    data = _note["data"]
+    file_id = _note["file_id"]
+    keyb = None
+    if data:       
         if findall(r"\[.+\,.+\]", data):
             keyboard = extract_text_and_keyb(ikb, data)
             if keyboard:
                 data, keyb = keyboard
+    replied_message = message.reply_to_message
+    if replied_message:
+        if replied_message.from_user.id != message.from_user.id:
+            message = replied_message
+    if type == "text":
         await message.reply_text(
-            data,
+            text=data,
             reply_markup=keyb,
             disable_web_page_preview=True,
         )
-    else:
-        await message.reply_sticker(_note["data"])
-
+    if type == "sticker":
+        await message.reply_sticker(
+            sticker=file_id,
+        )
+    if type == "animation":
+        await message.reply_animation(
+            animation=file_id,
+            caption=data,
+            reply_markup=keyb,
+        )
+    if type == "photo":
+        await message.reply_photo(
+            photo=file_id,
+            caption=data,
+            reply_markup=keyb,
+        )
+    if type == "document":
+        await message.reply_document(
+            document=file_id,
+            caption=data,
+            reply_markup=keyb,
+        )
+    if type == "video":
+        await message.reply_video(
+            video=file_id,
+            caption=data,
+            reply_markup=keyb,
+        )
+    if type == "video_note":
+        await message.reply_video_note(
+            video_note=file_id,
+        )
+    if type == "audio":
+        await message.reply_audio(
+            audio=file_id,
+            caption=data,
+            reply_markup=keyb,
+        )
+    if type == "voice":
+        await message.reply_voice(
+            voice=file_id,
+            caption=data,
+            reply_markup=keyb,
+        )
 
 @app2.on_message(
     filters.command("delete", prefixes=USERBOT_PREFIX)
